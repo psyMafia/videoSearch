c8bb05f972d922bc6bf3068ed6f70de3f3231e3e
psyMafia <51231411+psyMafia@users.noreply.github.com>
2020-10-03 13:55:19 +0300

    Updates

---
+++ precache-manifest.c32cf25710b087db6a717448443b51f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390ae8493b6e361177ada4250914a1b2",
+    "url": "/videoSearch/index.html"
+  },
+  {
+    "revision": "d94b8ffb71389d1f29bf",
+    "url": "/videoSearch/static/js/2.571c4728.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videoSearch/static/js/2.571c4728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c0077879e771513e99",
+    "url": "/videoSearch/static/js/main.5dc2049e.chunk.js"
+  },
+  {
+    "revision": "d4f4031b5dc22129164a",
+    "url": "/videoSearch/static/js/runtime-main.b7849497.js"
+  }
+]);
